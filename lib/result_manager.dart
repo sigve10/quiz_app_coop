a7236e654844dbec4267b2cd561a527f8af793d6
sigvebjo <sigve@bjorkedal.org>
2024-09-13 14:31:58 +0200

    Created result_manager.dart

---
+++ lib/result_manager.dart
@@ -0,0 +1,24 @@
+import 'package:quiz_app_coop/answer_set.dart';
+import 'package:quiz_app_coop/question.dart';
+import 'package:quiz_app_coop/question_set.dart';
+
+class ResultManager {
+  ResultManager();
+
+  int calcTotalPoints(QuestionSet questionSet, AnswerSet answerSet) {
+    int totalPoints = 0;
+
+    int length = questionSet.questions.length;
+    for (int index = 0; index < length; index++) {
+      Question question = questionSet.questions[index];
+      int correctIndex = question.correctIndex;
+      int selectedIndex = answerSet.answers[index];
+
+      if (correctIndex == selectedIndex) {
+        totalPoints += question.points;
+      }
+    }
+
+    return totalPoints;
+  }
+}
