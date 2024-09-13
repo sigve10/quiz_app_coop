d8fe84cbe45b4f0f340e3cbc77b49e0f1b780405
sigvebjo <sigve@bjorkedal.org>
2024-09-13 12:33:09 +0200

    Added documentation to answer_set and question_set

--- lib/answer_set.dart
+++ lib/answer_set.dart
@@ -1,15 +1,17 @@
+/// Primarily a wrapper for [List] in order to hold answers to [Question]s.
 class AnswerSet {
   late List<int> _answers;
 
+  /// Creates a new answer set of length [amount].
   AnswerSet(int amount) {
     _answers = <int>[amount];
   }
 
+  /// Sets the answer at [index] to the index [alternative],
+  /// which should correspond to a [Question]'s option.
   void setAnswer(int index, int alternative) {
-    _answers[index] = alternative;
+    answers[index] = alternative;
   }
 
-  List<int> getAnswers() {
-    return _answers;
-  }
+  List<int> get answers => _answers;
 }
--- lib/question_set.dart
+++ lib/question_set.dart
@@ -3,11 +3,28 @@ import 'package:flutter/services.dart' show rootBundle;
 import 'dart:convert';
 import 'question.dart';
 
+/// A set of [Question]s. Used primarily as a [List] wrapper,
+/// but also includes a [render] function.
 class QuestionSet {
   late List<Question> _questions;
 
+  /// Creates a new [QuestionSet] using a predefined list of [Question]s.
   QuestionSet(this._questions);
 
+  /// Creates a new [QuestionSet] from a JSON file at [pathName].
+  ///
+  /// JSON file should have a structure like:
+  /// ```
+  /// "questions": [
+  ///   {
+  ///     "text": ...,
+  ///     "options": [ "option", ... ],
+  ///     "correctIndex": 5,
+  ///     "points": 10
+  ///   },
+  ///   ...
+  /// ]
+  /// ```
   QuestionSet.import(String pathName) {
     _questions = <Question>[];
 
