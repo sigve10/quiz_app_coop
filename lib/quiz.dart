2279e39ada124f4da1828ebbe751120f813c9672
stenOskar <stenhalse@gmail.com>
2024-09-19 12:30:08 +0200

    Updated the quiz.dart with documentation

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -6,19 +6,22 @@ import 'package:quiz_app_coop/question_set.dart';
 import 'package:quiz_app_coop/result_manager.dart';
 import 'package:quiz_app_coop/result_screen.dart';
 
+/// This class represents a quiz made up of questions and answers.
 class Quiz {
   final AnswerSet answerSet;
   final QuestionSet questionSet;
 
   Quiz(this.answerSet, this.questionSet);
-
+  /// Renders [QuizWidget] with parameters [questionSet], and [answerSet]
   render() => QuizWidget(questionSet, answerSet);
 }
 
+/// Displays the quiz
 class QuizWidget extends StatefulWidget {
   final QuestionSet questionSet;
   final AnswerSet answerSet;
 
+  /// Constructor with [questionSet], [answerSet]
   const QuizWidget(
     this.questionSet,
     this.answerSet,
@@ -29,17 +32,21 @@ class QuizWidget extends StatefulWidget {
   State<StatefulWidget> createState() => QuizState();
 }
 
+/// Manage the state of quiz
 class QuizState extends State<QuizWidget> {
   int questionIndex = 0;
   List<Question> get questions => widget.questionSet.questions;
   final ScrollController _scrollController = ScrollController();
 
+  /// Decrease the current step with [setState] on [questionIndex]
   void _decreaseStep() {
     setState(() {
       questionIndex = max(0, questionIndex - 1);
     });
   }
 
+  /// Increase the current step by one,
+  /// checks if the quiz is on the last question. If so [_showResult]
   void _increaseStep() {
     setState(() {
       if (questionIndex == questions.length - 1) {
@@ -50,6 +57,7 @@ class QuizState extends State<QuizWidget> {
     });
   }
 
+  /// Displays the result when [_showResult] is called.
   void _showResult() {
     int totalPoints = ResultManager().calcTotalPoints(widget.questionSet, widget.answerSet);
 
@@ -63,7 +71,7 @@ class QuizState extends State<QuizWidget> {
     );
   }
 
-  ///
+  /// Resets [questionIndex] using method [reset] in [answer_set]
   void _resetQuiz() {
     setState(() {
       questionIndex = 0;
@@ -72,18 +80,22 @@ class QuizState extends State<QuizWidget> {
     Navigator.of(context).pop();
   }
 
+  /// Set state for stepper [questionIndex]
   void _setStep(index) {
     setState(() {
       questionIndex = index;
     });
   }
 
+  ///Set state for [_setAnswer] in [answerSet]
   void _setAnswer(int questionIndex, int answerIndex) {
     setState(() {
       widget.answerSet.setAnswer(questionIndex, answerIndex);
     });
   }
 
+  /// Creates [stepper] with [questionIndex], [_decreaseStep], [_increaseStep], [_setStep]
+  /// as it's variables
   Widget _createStepper() {
     return Stepper(
       controller: _scrollController,
@@ -103,6 +115,7 @@ class QuizState extends State<QuizWidget> {
     );
   }
 
+  /// Builds the UI on the state of questions.
   @override
   Widget build(BuildContext context) {
     return Scaffold(
